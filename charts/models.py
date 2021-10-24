48518f664d1d5b1dc9dd11038c03b41f3579fb4f
Trivium0911 <triv_1993@mail.ru>
2021-10-24 22:59:59 +0300

    Added cors-headers, whitenoise

--- charts/models.py
+++ charts/models.py
@@ -6,3 +6,5 @@ class LastUser(models.Model):
     user = models.TextField(unique= True)
     password = models.TextField(unique=True)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}(user = {self.user}, password= {self.password})"

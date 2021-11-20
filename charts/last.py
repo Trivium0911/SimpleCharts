ef5ac58360f97bed9bb412890258fa0260f01f00
Trivium0911 <triv_1993@mail.ru>
2021-11-20 16:45:47 +0300

    added top tracks, albums, artists for 6 month, 3 month, 1 months, 1 week

--- charts/last.py
+++ charts/last.py
@@ -3,37 +3,69 @@ import pylast
 from datetime import timedelta
 from django.contrib.auth import get_user_model
 import datetime
+from django.db.models import Count
 from charts.models import Chart
 
 
-
-
-
-
-# LAST_FM_SETTINGS
 User = get_user_model()
 chart = Chart.objects.all()
-lastfm_username = os.getenv("lastfm_username")
+lastfm_username = os.getenv('lastfm_username')
 
+# LAST_FM_SETTINGS
 API_KEY = os.getenv('API_KEY')
 API_SECRET = os.getenv('API_SECRET')
 lastfm_network = pylast.LastFMNetwork(
     api_key=API_KEY,
     api_secret=API_SECRET,
-    username=lastfm_username#"maximbreakdown",
-  # password_hash=lastfm_password_hash,
+    username = lastfm_username
 )
 
 date = round(datetime.datetime.now().timestamp())
 past_date = datetime.datetime.today() - timedelta(days=365)
 past_date = round(past_date.timestamp())
 
+def datefilter(cur_user,period):
+    user_charts = chart.filter(username=cur_user)
+    if not user_charts:
+        return None
+    startdate = user_charts[0].date_utc
+    enddate = startdate - timedelta(days=period)
+    filtered_top = user_charts.filter(date_utc__range=[enddate, startdate])
+    return filtered_top
+
+def get_user(request):
+    user = request.user
+    return user
+
+def chart_filter(cur_user):
+    filter = chart.filter(username= cur_user)
+    return filter
 
 def clear_user_db(cur_user):
     del_chart = chart.filter(username=cur_user)
     del_chart.delete()
     del_chart.save()
 
+def get_top_artists(cur_user,period):
+    artists = datefilter(cur_user,period)
+    if not artists:
+        return None
+    top_user_artists = artists.values('artist').annotate(count=Count('artist')).order_by("-count")
+    return top_user_artists
+
+def get_top_tracks(cur_user,period):
+    tracks = datefilter(cur_user,period)
+    if not tracks:
+        return None
+    top_user_tracks= tracks.values('artist', 'album','song_title').annotate(count=Count('song_title')).order_by("-count")
+    return top_user_tracks
+
+def get_top_albums(cur_user,period):
+    albums = datefilter(cur_user,period)
+    if not albums:
+        return None
+    top_user_albums = albums.values('artist', 'album').annotate(count=Count('album')).order_by("-count")
+    return top_user_albums
 
 def pack_to_db(cur_user):
     lastfm_network = pylast.LastFMNetwork(
@@ -42,6 +74,8 @@ def pack_to_db(cur_user):
         username=cur_user,
     )
     top_list = lastfm_network.get_user(cur_user).get_recent_tracks(time_from=past_date, time_to=date, limit=None)
+    if not top_list:
+        return None
     for top_item in top_list:
         date_utc = datetime.date.fromtimestamp(int(top_item.timestamp))
         chart = Chart(
@@ -55,6 +89,5 @@ def pack_to_db(cur_user):
         )
         chart.save()
 
-    if top_list:
-        return chart
+    return chart
 
--- charts/views.py
+++ charts/views.py
@@ -1,18 +1,16 @@
-from django.db.models import Count
 from django.shortcuts import render, redirect
+from pylast import PyLastError
 from charts.models import Chart
-from charts.last import pack_to_db, clear_user_db
+from charts.last import pack_to_db, clear_user_db, get_user
+from charts.last import get_top_artists, get_top_tracks, get_top_albums
+
 
 chart = Chart.objects.all()
-
-def get_user(request):
-    user = request.user
-    return user
 
 
 def chart_list(request):
     cur_user = get_user(request)
-    user_chart = chart.filter(username= cur_user)
+    user_chart = chart.filter(username = cur_user)
     if not user_chart:
         return render(request, 'charts/chart_list.html', { "no_charts": "You haven't charts"})
     first_date = user_chart.first()
@@ -27,7 +25,10 @@ def download_user_db(request):
             clear_user_db(cur_user)
         except AttributeError:
             pass
-        pack_to_db(cur_user)
+        try:
+            pack_to_db(cur_user)
+        except PyLastError:
+            return redirect("charts")
         return redirect("charts")
     return render(request, "charts/download.html")
 
@@ -46,18 +47,90 @@ def delete_user_db(request):
 
 def top_artists_year(request):
     cur_user = get_user(request)
-    user_artists = chart.filter(username = cur_user)
-    top_user_artists = user_artists.values('artist').annotate(count=Count('artist')).order_by("-count")
-    return render(request,"charts/top_artists/artists_year.html",{"top_user_artists" : top_user_artists})
+    period = 365
+    top_artists_year = get_top_artists(cur_user,period)
+    return render(request,"charts/top_artists/artists_year.html",{"top_artists_year" : top_artists_year})
 
 def top_tracks_year(request):
     cur_user = get_user(request)
-    user_chart = chart.filter(username = cur_user)
-    top_user_tracks = user_chart.values('artist', 'album','song_title').annotate(count=Count('song_title')).order_by("-count")
-    return render(request,"charts/top_tracks/tracks_year.html",{"top_user_tracks": top_user_tracks})
+    period = 365
+    top_tracks_year = get_top_tracks(cur_user,period)
+    return render(request,"charts/top_tracks/tracks_year.html",{"top_tracks_year": top_tracks_year})
 
 def top_albums_year(request):
     cur_user = get_user(request)
-    user_albums = chart.filter(username = cur_user)
-    top_user_albums = user_albums.values('artist', 'album').annotate(count=Count('album')).order_by("-count")
-    return render(request,"charts/top_albums/albums_year.html",{"top_user_albums" : top_user_albums})
+    period = 365
+    top_albums_year = get_top_albums(cur_user,period)
+    return render(request,"charts/top_albums/albums_year.html",{"top_albums_year" : top_albums_year})
+
+def top_artists_6month(request):
+    cur_user = get_user(request)
+    period = 183
+    top_artists_6month = get_top_artists(cur_user,period)
+    return render(request,"charts/top_artists/artists_6month.html",{"top_artists_6month" : top_artists_6month})
+
+def top_artists_3month(request):
+    cur_user = get_user(request)
+    period = 90
+    top_artists_3month = get_top_artists(cur_user,period)
+    return render(request,"charts/top_artists/artists_3month.html",{"top_artists_3month" : top_artists_3month})
+
+def top_artists_month(request):
+    cur_user = get_user(request)
+    period = 30
+    top_artists_month = get_top_artists(cur_user,period)
+    return render(request,"charts/top_artists/artists_3month.html",{"top_artists_month" : top_artists_month})
+
+def top_artists_week(request):
+    cur_user = get_user(request)
+    period = 7
+    top_artists_week = get_top_artists(cur_user,period)
+    return render(request,"charts/top_artists/artists_3month.html",{"top_artists_week" : top_artists_week})
+
+def top_albums_6month(request):
+    cur_user = get_user(request)
+    period = 183
+    top_albums_6month = get_top_albums(cur_user,period)
+    return render(request,"charts/top_albums/albums_6month.html",{"top_albums_6month" : top_albums_6month})
+
+def top_albums_3month(request):
+    cur_user = get_user(request)
+    period = 90
+    top_albums_3month = get_top_albums(cur_user,period)
+    return render(request,"charts/top_albums/albums_3month.html",{"top_albums_3month" : top_albums_3month})
+
+def top_albums_month(request):
+    cur_user = get_user(request)
+    period = 30
+    top_albums_month = get_top_albums(cur_user,period)
+    return render(request,"charts/top_albums/albums_month.html",{"top_albums_month" : top_albums_month})
+
+def top_albums_week(request):
+    cur_user = get_user(request)
+    period = 7
+    top_albums_week = get_top_albums(cur_user,period)
+    return render(request,"charts/top_albums/albums_week.html",{"top_albums_week" : top_albums_week})
+
+def top_tracks_6month(request):
+    cur_user = get_user(request)
+    period = 183
+    top_tracks_6month = get_top_tracks(cur_user,period)
+    return render(request,"charts/top_tracks/tracks_6month.html",{"top_tracks_6month": top_tracks_6month})
+
+def top_tracks_3month(request):
+    cur_user = get_user(request)
+    period = 90
+    top_tracks_3month = get_top_tracks(cur_user,period)
+    return render(request,"charts/top_tracks/tracks_3month.html",{"top_tracks_3month": top_tracks_3month})
+
+def top_tracks_month(request):
+    cur_user = get_user(request)
+    period = 30
+    top_tracks_month = get_top_tracks(cur_user,period)
+    return render(request,"charts/top_tracks/tracks_month.html",{"top_tracks_month": top_tracks_month})
+
+def top_tracks_week(request):
+    cur_user = get_user(request)
+    period = 7
+    top_tracks_week = get_top_tracks(cur_user,period)
+    return render(request,"charts/top_tracks/tracks_week.html",{"top_tracks_week": top_tracks_week})

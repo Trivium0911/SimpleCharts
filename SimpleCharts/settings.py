d5feef6e438093475083490d04952d4ea1e1355b
Trivium0911 <triv_1993@mail.ru>
2021-11-24 10:25:48 +0300

    update: added static root

--- SimpleCharts/settings.py
+++ SimpleCharts/settings.py
@@ -111,8 +111,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = BASE_DIR.parent / ".static"
 
 STATIC_URL = '/static/'
 if not DEBUG:

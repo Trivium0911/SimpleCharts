8cb3221f4f58e9958cb7bbed71822c36cc263b83
Trivium0911 <triv_1993@mail.ru>
2021-11-24 12:44:08 +0300

    update: try to break into pieces request to last.fm

--- charts/last.py
+++ charts/last.py
@@ -23,7 +23,8 @@ lastfm_network = pylast.LastFMNetwork(
 )
 
 date = round(datetime.datetime.now().timestamp())
-past_date = datetime.datetime.today() - timedelta(days=183)
+delta = timedelta(days=92)
+past_date = datetime.datetime.today() - delta
 past_date = round(past_date.timestamp())
 
 def pagination(request,top):
@@ -77,24 +78,29 @@ def pack_to_db(cur_user):
         api_secret=API_SECRET,
         username=cur_user,
     )
-    top_list = lastfm_network.get_user(cur_user).get_recent_tracks(time_from=past_date, time_to=date, limit=None)
+    top_list = []
+    top_list1 = lastfm_network.get_user(cur_user).get_recent_tracks(time_from=past_date, time_to=date, limit=None)
+    top_list.append(top_list1)
+    top_list2 = lastfm_network.get_user(cur_user).get_recent_tracks(time_from=past_date - delta, time_to=past_date, limit=None)
+    top_list.append(top_list2)
     if top_list == []:
         new_date = int(lastfm_network.get_user(cur_user).get_recent_tracks(limit=1)[0][3])
         new_past_date = str(datetime.date.fromtimestamp(new_date) - timedelta(days=365))
         new_past_date = int(time.mktime(time.strptime(new_past_date, '%Y-%m-%d')))
         top_list = lastfm_network.get_user(cur_user).get_recent_tracks(time_from=new_past_date, time_to=new_date, limit=None)
-    for top_item in top_list:
-        date_utc = datetime.date.fromtimestamp(int(top_item.timestamp))
-        chart = Chart(
-            username= cur_user,
-            artist=top_item.track.artist,
-            song_title=top_item.track.title,
-            album=top_item.album,
-            date=top_item.playback_date,
-            date_utc=date_utc,
+    for i in top_list:
+        for top_item in i:
+            date_utc = datetime.date.fromtimestamp(int(top_item.timestamp))
+            chart = Chart(
+                username= cur_user,
+                artist=top_item.track.artist,
+                song_title=top_item.track.title,
+                album=top_item.album,
+                date=top_item.playback_date,
+                date_utc=date_utc,
 
-        )
-        chart.save()
+            )
+            chart.save()
 
     return chart
 

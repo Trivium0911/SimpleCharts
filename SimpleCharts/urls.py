c91a8061cc87d74195939374344588b6ccf49b93
Trivium0911 <triv_1993@mail.ru>
2022-05-07 10:06:05 +0300

    update

--- SimpleCharts/urls.py
+++ SimpleCharts/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
-from SimpleCharts.views import SignUpView, SimpleCharts, start
+from SimpleCharts.views import SignUpView, simplecharts, start
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('start/charts/', include("charts.urls"), name = "charts"),
-    path ("start/", SimpleCharts , name='simplecharts'),
-    path ("start/signup/", SignUpView.as_view(),name='signup'),
-    path ("start/login/", LoginView.as_view(),name='login'),
-    path ("logout/", LogoutView.as_view(), name='logout'),
-    path ("", start, name='start')
+    path('start/charts/', include("charts.urls"), name="charts"),
+    path("start/", simplecharts, name='simplecharts'),
+    path("start/signup/", SignUpView.as_view(), name='signup'),
+    path("start/login/", LoginView.as_view(), name='login'),
+    path("logout/", LogoutView.as_view(), name='logout'),
+    path("", start, name='start')
     ]
